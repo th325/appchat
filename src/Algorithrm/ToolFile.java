1dfd7d8841ffe8e89f81533e3be797a6bf3a1178
th325 <huynhthuchct@gmail.com>
2019-03-25 00:28:01 +0700

    12:27

--- src/Algorithrm/ToolFile.java
+++ src/Algorithrm/ToolFile.java
@@ -25,101 +25,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
 
-/**
- *
- * @author Huynh
- */
 public class ToolFile {
    public void copyFile(String src,String dest) throws IOException{
         Path source = Paths.get(src);
 		// Destination file.
 	Path destination = Paths.get(dest);
        Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-    }
-   public static  void AppendByte( File file_name,int size) throws FileNotFoundException, IOException{
-    String str = "World";
-    BufferedWriter writer = new BufferedWriter(new FileWriter(file_name, true));
-    writer.append(' ');
-    writer.append(str);
-    writer.close(); 
-   }
-   public static String fillString(char fillChar, int count){  
-       // creates a string of 'x' repeating characters  
-       char[] chars = new char[count];  
-       java.util.Arrays.fill(chars, fillChar);  
-       return new String(chars);  
-   }
-    public static void runfile() throws IOException{
-        
-         XWPFDocument document = new XWPFDocument(new FileInputStream("123.docx"));
-       //Blank Document
-       //Write the Document in file system
-       //create Paragraph
-       try ( //Write the Document in file system
-               FileOutputStream out = new FileOutputStream( new File("createdocument.docx"))) {
-           //Blank Document
-           //Write the Document in file system
-           //create Paragraph
-           File getLengOriginalFile = new File("123.docx"); 
-           int length2 = (int)getLengOriginalFile.length();
-           int msize = (int)1024-length2%1024;
-           String field=fillString('\u200e',msize);
-           //for(int i=0;i<msize;i++)field+="c";
-//           XWPFParagraph paragraph = document.createParagraph();
-//           XWPFRun run = paragraph.createRun();
-//           run.setText(field);
-           //
-           document.write(out);
-       }
-    }
-    public static void main(String[] args) throws IOException{
-          // TODO code application logic here
-        //Blank Document
-        runfile();
-        
-      
-//        XWPFDocument document = new XWPFDocument(new FileInputStream("createdocument.docx"));
-//      //Write the Document in file system
-//        FileOutputStream out = new FileOutputStream( new File("createdocument1.docx"));   
-        //Blank Document    
-      //Write the Document in file system       
-      //create Paragraph
-      File getLengOriginalFile = new File("createdocument.docx");  
-      int length2 = (int)getLengOriginalFile.length();
-      int count  = 0;
-      while(length2 % 1024 != 0){
-        XWPFDocument document = new XWPFDocument(new FileInputStream("createdocument.docx"));
-      //Write the Document in file system
-        FileOutputStream out = new FileOutputStream( new File("createdocument.docx"));  
-        XWPFParagraph paragraph = document.createParagraph();
-        XWPFRun run = paragraph.createRun();
-       
-        run.setText(" ");
-        //
-        
-        document.write(out);
-        out.close();
-        File filess = new File("createdocument.docx");       
-        length2 = (int)filess.length();
-        System.out.println(filess.length());
-        count++;
-        
-      }
-        runfile();
-        XWPFDocument document = new XWPFDocument(new FileInputStream("createdocument.docx"));
-        FileOutputStream out = new FileOutputStream( new File("createdocument.docx"));  
-        XWPFParagraph paragraph = document.createParagraph();
-        XWPFRun run = paragraph.createRun();
-
-        run.setText(fillString(' ', count));
-        document.write(out);
-        out.close();
-        
-        File filess = new File("createdocument.docx");       
-        System.out.println(filess.length());
     }
 }
